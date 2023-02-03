29bff9df46079ba49bd5fa808ccc198d0f58f3cc
Ginachiko1998 <soniagina669@gmail.com>
2023-02-03 21:21:03 +0700

    make ecommerceapp

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Ecommerce App 2023 ©copyright</p>
+    </div>
+  );
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../App.css"
+
+const Navbar = ({setToken}) => {
+
+  const logOutHandler = () => {
+    setToken ("");
+    localStorage.clear();
+  };
+  return (
+    <div className="navbar">
+      <h1>Ecommerce App</h1>
+      <button className="log-out-btn" onClick={()=>logOutHandler ()}>
+        Log Out
+      </button>
+    </div>
+  );
+}
+
+export default Navbar
