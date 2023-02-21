898d67712bfd319ac12bffceb9d0b5749c582497
Ginachiko1998 <soniagina669@gmail.com>
2023-02-22 01:52:36 +0700

    make change in navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer/Footer';
-import Products from './components/Product';
-import Login from './components/Login';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer/Footer";
+import Products from "./components/Product";
+import Login from "./components/Login";
 import { useState } from "react";
 
 function App() {
-  const [token,setToken] = useState(localStorage.getItem("userToken") ?? null);
-  
+  const [token, setToken] = useState(localStorage.getItem("userToken") ?? null);
+
   return (
     <div className="App">
-      <Navbar setToken={setToken}/>
-      {token ? <Products /> : <Login token={token} setToken={setToken}/>}
-      <Footer/>
+      <Navbar setToken={setToken} />
+      {token ? <Products /> : <Login token={token} setToken={setToken} />}
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,39 @@
-import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
-// import sun from "../assets/sunlogo.jpg";
+import UserList from './UserList';
+import React, { useState, useEffect } from "react";
 
 
-const Header= ({ setToken }) => {
+function Header({ setToken }) {
   const logOutHandler = () => {
     setToken("");
     localStorage.clear();
   };
+  const [users, setUsers] = useState([
+    { name: 'mor_2314' },
+  ]);
+
   return (
     <header className="header">
       <div className="header__logo">
-      <div className="flex items-center">
-      {/* <img className="img-fluid bg-transparent logo" src={sun} alt="e-commerce sun" /> */}
-      <div className="self-center fw-bold white space-nowrap text-white ms-4 fs-5 ">
-      Sun Shop
+        <div className="flex items-center">
+          {/* <img className="img-fluid bg-transparent logo" src={sun} alt="e-commerce sun" /> */}
+          <div className="self-center fw-bold white space-nowrap text-white ms-4 fs-5 ">
+            Sun Shop
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-        <nav className="header__nav me-5">
+      <nav className="header__nav me-5">
         <ul>
           <li className="text-white">Home</li>
-          <li className="text-white">Service</li>
+          <li className="text-white">About</li>
           <li className="text-white">Contact</li>
+          <li className="text-white"><UserList users={users} /> </li>
           <li className="text-white" onClick={() => logOutHandler()}>Logout</li>
         </ul>
-        </nav>
+      </nav>
     </header>
   );
-};
+}
 
 export default Header;
